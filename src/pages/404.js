8fad909181153de95c9ffc00d34774086d29a189
Amar <74207398+AmarOfficiel@users.noreply.github.com>
2024-06-03 21:38:18 +0200

    Fix deploy Vercel

--- src/pages/404.js
+++ src/pages/404.js
@@ -18,11 +18,11 @@ export default function Custom404() {
             </h2>
             <section className={` ${styles.itemHistoryDescription}`}>
               <p>
-                The term "404 Not Found" was coined by Berners-Lee himself, who
-                explained in a 1998 interview that he wanted to make the error
-                message "slightly apologetic". He also said that he considered
-                using "400 Bad Request" instead, but decided that it was too
-                vague and technical.
+                The term &ldquo;404 Not Found&ldquo; was coined by Berners-Lee
+                himself, who explained in a 1998 interview that he wanted to
+                make the error message &ldquo;slightly apologetic&ldquo;. He
+                also said that he considered using &ldquo;400 Bad Request&ldquo;
+                instead, but decided that it was too vague and technical.
               </p>
               <p>
                 The first documented case of a 404 error appearing on a web page
@@ -30,23 +30,24 @@ export default function Custom404() {
                 web browser on the NCSA website. The page had been moved to a
                 different location, but the link had not been updated. The user
                 reported the error to the NCSA team, who fixed the link and
-                added a humorous message to their 404 page: "We're sorry, but
-                the document you requested is not here. Maybe you should try
-                someplace else."
+                added a humorous message to their 404 page: &ldquo;We&apos;re
+                sorry, but the document you requested is not here. Maybe you
+                should try someplace else.&ldquo;
               </p>
               <p>
                 Since then, 404 errors have become one of the most common and
                 recognizable errors on the Web. Many websites have customized
                 their 404 pages with creative designs, messages, or features to
-                entertain or assist their visitors. For example, Google's 404
-                page features a broken robot and a link to its homepage, while
-                GitHub's 404 page shows a random image of a parallax star field
-                and a link to its status page. Some websites have also used
-                their 404 pages to showcase their brand personality, humor, or
-                social causes. For instance, Lego's 404 page shows the Lego
-                minifigure Emmet along with a humorous message, Amazon's
-                displays the image of a dog, Peugeot's shows a picture of its
-                404 model, and RTÉ's shows an image of Bosco.
+                entertain or assist their visitors. For example, Google&apos;s
+                404 page features a broken robot and a link to its homepage,
+                while GitHub&apos;s 404 page shows a random image of a parallax
+                star field and a link to its status page. Some websites have
+                also used their 404 pages to showcase their brand personality,
+                humor, or social causes. For instance, Lego&apos;s 404 page
+                shows the Lego minifigure Emmet along with a humorous message,
+                Amazon&apos;s displays the image of a dog, Peugeot&apos;s shows
+                a picture of its 404 model, and RTÉ&apos;s shows an image of
+                Bosco.
               </p>
               <Link
                 target="_blank"
